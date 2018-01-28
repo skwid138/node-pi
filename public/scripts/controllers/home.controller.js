2715f1e2dd2f5e06d9982f413eeb603a5c25f4f5
Hunter Rancourt <skwid138@gmail.com>
2018-01-28 16:25:06 -0600

    sourced in angularjs, material, and setup client js and home controller

---
+++ public/scripts/controllers/home.controller.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+/*
+This controller is for the home view.
+- displays 
+*/
+myApp.controller('HomeController', function (GithubService, MailService, $mdDialog) {
+    console.log('in HomeController');
+    const vm = this;
+
+    
+
+}); // end HomeController
