f82f5cc7dc84ae68a1a502c169e53ff06b0d9546
Hunter Rancourt <skwid138@gmail.com>
2018-01-28 17:09:29 -0600

    fixed some bugs and added script for testing socket.io

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -4,10 +4,10 @@
 This controller is for the home view.
 - displays 
 */
-myApp.controller('HomeController', function (GithubService, MailService, $mdDialog) {
+myApp.controller('HomeController', function (  ) {
     console.log('in HomeController');
     const vm = this;
 
-    
+
 
 }); // end HomeController
